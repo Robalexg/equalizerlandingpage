423d4d95bf7b655379cede90b1eee1b22846c00b
robalexg <alexg7070@yahoo.com>
2022-11-13 22:51:15 -0600

    fixing responsive ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 import './App.css';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
         <Header/>
+        {/* <Main/> */}
     </div>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,37 @@
+import './Main.css'
+
+const Main = () => {
+    return(
+        <main className='main--container'>
+            <div className='main--bg'>
+                <img className='main--phone' src="../images/illustration-app.png" alt="" />
+                {/* <div className='main--card'>
+                    <h1>Premium EQ</h1>
+                    <h2>
+                        Get expert-level control with a 
+                        robust equalizer, volume mixer, 
+                        and spatial audio. Take your listening 
+                        experience to a whole new level and 
+                        access all our incredible features!
+                    </h2>
+                    <span>$4 /month</span>
+                    <button>
+                        <span>
+                            <img src="../images/icon-apple.svg" alt="" />
+                        </span>
+                        iOS Download
+                    </button>
+                    <button>
+                        <span>
+                            <img src="../images/icon-android.svg" alt="" />
+                        </span>
+                        Android Download
+                    </button>
+                </div> */}
+            </div>
+            {/* <img className='main--phone' src="../images/illustration-app.png" alt="" /> */}
+        </main>
+    )
+}
+
+export default Main
